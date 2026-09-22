12d5e5e4cc2c943870275d8999407b2a3b80f8d6
Fortunatus Olorunsola <fortunatus.olorunsola@gmail.com>
2026-09-22 21:17:13 +0100

    Encrypt message

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,7 +1,3 @@
-def caesar_cipher(message, right_shift)
-  # "\"#{message}\" shifted by #{right_shift} letters."
-end
-
 def getMessage()
   puts "What message would you like to encrypt?"
   message = gets.chomp
@@ -14,6 +10,33 @@ def getRightShift()
   right_shift
 end
 
+def right_shift(character, shift)
+  capital_letters = ("A".."Z").to_a
+  small_letters = ("a".."z").to_a
+  alphabets = 26
+
+  if capital_letters.include?(character) 
+    position = capital_letters.index(character)
+    return capital_letters[(position + shift) % alphabets]
+  end
+  
+  if small_letters.include?(character) 
+    position = small_letters.index(character)
+    return small_letters[(position + shift) % alphabets]
+  end
+end
+
+def caesar_cipher(message, right_shift)
+  characters = message.split("")
+  
+  characters.each_with_index do |character, idx|
+    if character.between?("a", "z") || character.between?("A", "Z")
+      characters[idx] = right_shift(character, right_shift)
+    end
+  end
+  characters.join
+end
+
 
 encrypted_message = caesar_cipher(getMessage(), getRightShift())
-puts "Your encrypted message is:\n#{encrypted_message}."
+puts "\nYour encrypted message is:\n\"#{encrypted_message}\""
